d60abd5137758f9ea7707dbc20d9055b0255c900
harshalpatond1 <harshalpatond143@gmail.com>
2026-09-03 16:32:25 +0530

    connect api model

--- resume_parser.py
+++ resume_parser.py
@@ -5,3 +5,11 @@ from dotenv import load_dotenv
 from groq import Groq
 from pydantic import BaseModel, Field
 
+load_dotenv()
+my_api_key=os.getenv("GROQ_API_KEY")
+
+if not my_api_key:
+    raise ValueError("API key kaha hai bhai")
+
+client=Groq(api_key=my_api_key)
+model = "openai/gpt-oss-120b"
